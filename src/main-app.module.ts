555d31c6c7191b1e6dac2eb1b461511cd14ac449
HeL <aqhelaja@gmail.com>
2025-01-08 00:00:00 +0700

    Update: src/main-app.module.ts

--- src/main-app.module.ts
+++ src/main-app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppModule } from "./modules/app/app.module";
 import { SampleModule } from './modules/sample/sample.module';
+import { DatabaseModule } from './core/database/database.module';
+import { SampleEmployeesModule } from './modules/sample-employees/sample-employees.module';
 
 @Module({
-  imports: [AppModule, SampleModule],
+  imports: [AppModule, SampleModule, DatabaseModule, SampleEmployeesModule],
   controllers: [],
   providers: [],
 })
