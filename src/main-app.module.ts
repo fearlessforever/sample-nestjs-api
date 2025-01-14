24e0733a07d93a8b6d1b83638b61d4c3f52f5957
HeL <aqhelaja@gmail.com>
2025-01-15 00:00:00 +0700

    Update: src/main-app.module.ts

--- src/main-app.module.ts
+++ src/main-app.module.ts
@@ -3,9 +3,10 @@ import { AppModule } from "./modules/app/app.module";
 import { SampleModule } from './modules/sample/sample.module';
 import { DatabaseModule } from './core/database/database.module';
 import { SampleEmployeesModule } from './modules/sample-employees/sample-employees.module';
+import { FileLoggerModule } from "./core/file-logger/file-logger.module";
 
 @Module({
-  imports: [AppModule, SampleModule, DatabaseModule, SampleEmployeesModule],
+  imports: [AppModule, SampleModule, DatabaseModule, SampleEmployeesModule, FileLoggerModule],
   controllers: [],
   providers: [],
 })
