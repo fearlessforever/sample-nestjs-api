b3b51edb9c64c4e10bf5334dccd1b5788cde05d1
HeL <aqhelaja@gmail.com>
2025-01-18 00:00:00 +0700

    Update: src/modules/sample-employees/sample-employees.controller.ts

--- src/modules/sample-employees/sample-employees.controller.ts
+++ src/modules/sample-employees/sample-employees.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
 import { SampleEmployeesService } from './sample-employees.service';
 import { SampleEmployee, SampleEmployeeStatusDTO, UpdateSampleEmployeeDto } from '@DTO/sample-employee';
+import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 
 @Controller('sample-employees')
 export class SampleEmployeesController {
   constructor(private readonly sampleEmployeesService: SampleEmployeesService) {}
 
+  @Throttle({ long:{ limit:3 , ttl: 60_000 } }) // replace value for long key , in 1 minute allow only 3
+  @UseGuards(ThrottlerGuard)
   @Post()
   create(@Body() data: SampleEmployee) {
     return this.sampleEmployeesService.create(data);
   }
 
+  @UseGuards(ThrottlerGuard)
   @Get()
   findAll( @Query() queryParam: SampleEmployeeStatusDTO ) {
     // queryParam.role will now be a strongly-typed 'Role' enum value
@@ -22,6 +26,7 @@ export class SampleEmployeesController {
     return this.sampleEmployeesService.findOne(+id);
   }
 
+  @UseGuards(ThrottlerGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() data: UpdateSampleEmployeeDto ) {
     return this.sampleEmployeesService.update(+id, data);
