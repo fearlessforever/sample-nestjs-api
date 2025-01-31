bedc74fa6f165befbecbb8c33210ff59a8d2af38
HeL <aqhelaja@gmail.com>
2025-02-01 00:00:00 +0700

    Update: src/main-app.module.ts

--- src/main-app.module.ts
+++ src/main-app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from "@nestjs/common";
-import { AppModule } from "./modules/app/app.module";
-import { SampleModule } from './modules/sample/sample.module';
 import { DatabaseModule } from './core/database/database.module';
-import { SampleEmployeesModule } from './modules/sample-employees/sample-employees.module';
 import { FileLoggerModule } from "./core/file-logger/file-logger.module";
 import { RateLimiterModule } from './core/rate-limiter/rate-limiter.module';
 import { MyAppFilterModule } from './core/filters/my-app-filter.module';
-import { SampleCacheUsageModule } from './modules/sample-cache-usage/sample-cache-usage.module';
 import { MyAppCacheModule } from "./core/database/my-app-cache.module";
+import { RoutesModule } from "./routes/module.routes";
 
 @Module({
-  imports: [AppModule, SampleModule, DatabaseModule, SampleEmployeesModule, FileLoggerModule, RateLimiterModule, MyAppFilterModule, MyAppCacheModule, SampleCacheUsageModule],
+  imports: [ DatabaseModule , FileLoggerModule, RateLimiterModule, MyAppFilterModule, MyAppCacheModule, RoutesModule],
   controllers: [],
   providers: [],
 })
