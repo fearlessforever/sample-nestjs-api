d35c72b2e21b7afa7922e85ebce3c9a83fb0327b
HeL <aqhelaja@gmail.com>
2025-02-06 00:00:00 +0700

    Add: src/routes/sample-employees/sample-employees.controller.spec.ts

---
+++ src/routes/sample-employees/sample-employees.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SampleEmployeesController } from './sample-employees.controller';
+import { SampleEmployeesService } from './sample-employees.service';
+
+describe('SampleEmployeesController', () => {
+  let controller: SampleEmployeesController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [SampleEmployeesController],
+      providers: [SampleEmployeesService],
+    }).compile();
+
+    controller = module.get<SampleEmployeesController>(SampleEmployeesController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
