27563a0c170995c11d9850a3c1dbe1ee1f16bb81
HeL <aqhelaja@gmail.com>
2025-02-10 00:00:00 +0700

    Update: src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { MainAppModule } from './main-app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { FileLoggerService } from './core/file-logger/file-logger.service';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { setAppViewsForExpressAdapter } from './views/views.configs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MainAppModule);
+  const app = await NestFactory.create<NestExpressApplication>(MainAppModule);
 
   // add validation pipe tranformer
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
   }))
+  
+  setAppViewsForExpressAdapter(app , __dirname)
 
   if( process.env.IS_FORWARD_LOG_TO_FILE === 'true' )
     app.useLogger(app.get(FileLoggerService))
