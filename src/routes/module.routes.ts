b05a72acea0e5234178a5b64d2a3cc2de6b9801b
HeL <aqhelaja@gmail.com>
2025-02-13 00:00:00 +0700

    Update: src/routes/module.routes.ts

--- src/routes/module.routes.ts
+++ src/routes/module.routes.ts
@@ -4,10 +4,11 @@ import { SampleEmployeesModule } from "./sample-employees/sample-employees.modul
 import { SampleModule } from "./sample/sample.module";
 import { AppModule } from "./app/app.module";
 import { SampleViewMvcModule } from "./sample-view-mvc/sample-view-mvc.module";
+import { SampleSseModule } from "./sample-sse/sample-sse.module";
 
 
 @Module({
-  imports: [AppModule, SampleModule, SampleEmployeesModule, SampleCacheUsageModule, SampleViewMvcModule],
+  imports: [AppModule, SampleModule, SampleEmployeesModule, SampleCacheUsageModule, SampleViewMvcModule, SampleSseModule],
   controllers: [],
   providers: [],
 })
