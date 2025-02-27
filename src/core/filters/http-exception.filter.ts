27c4454f32ffe184ea910ad2fe77377a8293aa34
HeL <aqhelaja@gmail.com>
2025-02-28 00:00:00 +0700

    Update: src/core/filters/http-exception.filter.ts

--- src/core/filters/http-exception.filter.ts
+++ src/core/filters/http-exception.filter.ts
@@ -1,4 +1,4 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { ArgumentsHost, BadRequestException, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
 import { FileLoggerService } from '../file-logger/file-logger.service';
 import { Request, Response } from 'express';
 
@@ -14,14 +14,20 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
+    let message: string|string[] = `${exception.message}`
 
     this.logger.error(`[HttpException] ${status} ${request.url} : ${exception.message}`)
+
+    if( exception instanceof BadRequestException ){
+      const currentResponse = exception.getResponse() as any
+      message = currentResponse?.message || message
+    }
 
     response
       .status(status)
       .json({
         statusCode: status,
-        message: exception.message,
+        message: message,
         timestamp: new Date().toISOString(),
         path: request.url,
       });
