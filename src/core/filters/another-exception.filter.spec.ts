b4f097508734c6a2067283753d54e440eea40c6d
HeL <aqhelaja@gmail.com>
2025-03-09 00:00:00 +0700

    Update: src/core/filters/another-exception.filter.spec.ts

--- src/core/filters/another-exception.filter.spec.ts
+++ src/core/filters/another-exception.filter.spec.ts
@@ -1,7 +1,8 @@
+import { FileLoggerService } from '../file-logger/file-logger.service';
 import { AnotherExceptionFilter } from './another-exception.filter';
 
 describe('AnotherExceptionFilter', () => {
   it('should be defined', () => {
-    expect(new AnotherExceptionFilter()).toBeDefined();
+    expect(new AnotherExceptionFilter(new FileLoggerService())).toBeDefined();
   });
 });
