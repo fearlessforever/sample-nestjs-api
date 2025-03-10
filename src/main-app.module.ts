55c417c1aa97abf519f5d5fdaa6ad050609557ba
HeL <aqhelaja@gmail.com>
2025-03-11 00:00:00 +0700

    Update: src/main-app.module.ts

--- src/main-app.module.ts
+++ src/main-app.module.ts
@@ -1,15 +1,22 @@
 import { Module } from "@nestjs/common";
-import { DatabaseModule } from './core/database/database.module';
+import { DatabaseModule } from './database/database.module';
+import { MyAppCacheModule } from "./database/my-app-cache.module";
 import { FileLoggerModule } from "./core/file-logger/file-logger.module";
 import { RateLimiterModule } from './core/rate-limiter/rate-limiter.module';
 import { MyAppFilterModule } from './core/filters/my-app-filter.module';
-import { MyAppCacheModule } from "./core/database/my-app-cache.module";
 import { RoutesModule } from "./routes/module.routes";
 import { SocketModule } from './core/socket/socket.module';
 import { QueueModule } from "./core/queue/queue.module";
+import { ConfigsModule } from "./core/configs/configs.module";
 
 @Module({
-  imports: [SocketModule.forRoot(), QueueModule.forRoot(), DatabaseModule , FileLoggerModule, RateLimiterModule, MyAppFilterModule, MyAppCacheModule, RoutesModule],
+  imports: [
+    ConfigsModule.forRoot(), 
+    SocketModule.forRoot(), 
+    QueueModule.forRoot(), 
+    DatabaseModule.forRoot() , 
+    FileLoggerModule, RateLimiterModule, MyAppFilterModule, MyAppCacheModule, RoutesModule
+  ],
   controllers: [],
   providers: [],
 })
